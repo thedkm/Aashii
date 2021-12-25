36120b84a850d7926154b4346667ff3318f3ee36
Ƭƴɭ៩Ʀ Ɖµɾ∂εɳ <16339128+thedkm@users.noreply.github.com>
2021-12-25 23:50:38 +0530

    Update message.py

--- Aashii/constants/message.py
+++ Aashii/constants/message.py
@@ -92,7 +92,8 @@ class Message:
         "Welcome to @TheEolianSupportBot 👋"
         "Feel Free to ask any questions related to the group and any suggestions.\n\n"
         "Just send me any message you wish to inform the admins and As soon as an admin connects with you, you'll receive a notification.\n\n"
-        "Open @eolianinvitelinkbot and Click /invite if you want to join the The Eolian Group."
+        "Open @eolianinvitelinkbot and Click /invite if you want to join the The Eolian Group.\n\n"
+        "Do not request books through this bot!"
 
     )
 
